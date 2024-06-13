8fea7a3c6328e9dc9fb791edde868c6e9c7daf65
Ender Goyo <ing.endergoyo@gmail.com>
2024-06-13 15:34:58 -0400

    empesamos

---
+++ entradas_datos.py
@@ -0,0 +1,10 @@
+palabra = input ("Introduce una palabra: ")
+num_int = int(input("Introduce un numero entero: "))
+num_float = float(input("Introduce un numero flotante: "))
+num_compless = complex(input("Introduce un numero Complejo: "))
+
+print("string: ", palabra)
+print("entero: ", num_int)
+print("flotante: ", num_float)
+print("numero complejo: ", num_compless)
+
---
+++ entradas_datos2.py
@@ -0,0 +1,11 @@
+
+print("holaa ya camibie")
+nombre = input("Cual es tu nombre?: ")
+print ("hola "+ nombre + ",Vamos a realizar una suma.")
+
+num_1 = int(input("Por favor introduce el primer valor: "))
+num_2 = int(input("Por favor introduce el Segundo valor: "))
+
+resulf = num_1 + num_2
+print("El resultado de la suma es: ",resulf)
+print("presione enter para salir")
